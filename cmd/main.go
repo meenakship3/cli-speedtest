f76a6ff1cb644ca8105caad52fac0ef67fcfc9b3
meenakship3 <meenakshi.pradeep1@gmail.com>
2026-01-06 00:16:22 +0530

    basic speedtest

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,43 @@ import (
 	"github.com/showwin/speedtest-go/speedtest"
 )
 
-func main() {
+func checkSpeed() (float64, float64, error) {
 	var speedtestClient = speedtest.New()
-	serverList, _ := speedtestClient.FetchServers()
-	targets, _ := serverList.FindServer([]int{})
+
+	serverList, err := speedtestClient.FetchServers()
+	if err != nil {
+		fmt.Println(err)
+		return 0, 0, err
+	}
+
+	targets, err := serverList.FindServer([]int{})
+	if err != nil {
+		fmt.Println(err)
+		return 0, 0, err
+	}
+
+	var downloadSpeed, uploadSpeed float64
 
 	for _, s := range targets {
 		s.PingTest(nil)
 		s.DownloadTest()
 		s.UploadTest()
 		fmt.Printf("Latency: %s, Download: %s, Upload: %s\n", s.Latency, s.DLSpeed, s.ULSpeed)
+		downloadSpeed = s.DLSpeed.Mbps()
+		uploadSpeed = s.ULSpeed.Mbps()
 		s.Context.Reset()
 	}
+	return downloadSpeed, uploadSpeed, nil
+}
+
+func main() {
+	downloadSpeed, uploadSpeed, err := checkSpeed()
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	if downloadSpeed < 50.00 { // TODO: remove hardcoding
+		fmt.Println("Download speed is below 50 mbps")
+	}
+	fmt.Print(downloadSpeed, uploadSpeed, err)
 }
