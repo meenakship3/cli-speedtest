823e80a370b5f6c7df93637e03ff6fb6f5b00493
meenakship3 <meenakshi.pradeep1@gmail.com>
2026-01-06 19:42:28 +0530

    added flags + slack notifications

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/showwin/speedtest-go/speedtest"
 )
+
+func getFlags() (float64, float64, bool, float64, bool) {
+	dlT := flag.Float64("download-threshold", 50.00, "sets alert threshold (mbps) for download speeds")
+	ulT := flag.Float64("upload-threshold", 50.00, "sets alert threshold (mbps) for upload speeds")
+	ulM := flag.Bool("upload-monitoring", false, "checks upload speed along with download speed")
+	interval := flag.Float64("interval", 60.00, "sets check interval (in minutes)")
+	once := flag.Bool("run-once", true, "sets test frequency")
+
+	flag.Parse()
+
+	return *dlT, *ulT, *ulM, *interval, *once
+}
 
 func checkSpeed() (float64, float64, error) {
 	var speedtestClient = speedtest.New()
@@ -35,14 +56,77 @@ func checkSpeed() (float64, float64, error) {
 	return downloadSpeed, uploadSpeed, nil
 }
 
-func main() {
-	downloadSpeed, uploadSpeed, err := checkSpeed()
+func getWebhookURL() string {
+	err := godotenv.Load()
 	if err != nil {
-		fmt.Print(err)
-		return
+		log.Fatal("Error loading .env file")
 	}
-	if downloadSpeed < 50.00 { // TODO: remove hardcoding
-		fmt.Println("Download speed is below 50 mbps")
+	webhookURL, isSet := os.LookupEnv("WEBHOOK_URL")
+	if !isSet || webhookURL == "" {
+		log.Fatal("Error getting Slack Webhook URL")
 	}
-	fmt.Print(downloadSpeed, uploadSpeed, err)
+	return webhookURL
+}
+func notify(messageContent string) string {
+	webhookURL := getWebhookURL()
+
+	postBody, _ := json.Marshal(map[string]string{
+		"text": messageContent,
+	})
+
+	responseBody := bytes.NewBuffer(postBody)
+	resp, err := http.Post(webhookURL, "application/json", responseBody)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sb := string(body)
+	return sb
+}
+
+func getCurrentDateTime() string {
+	loc, err := time.LoadLocation("Asia/Kolkata")
+	if err != nil {
+		fmt.Println("Error loading location:", err)
+		return ""
+	}
+
+	now := time.Now().In(loc)
+
+	formattedTime := now.Format("02/01/2006 15:04:05 MST")
+	return formattedTime
+}
+
+func main() {
+	downloadThreshold, uploadThreshold, uploadMonitoring, interval, once := getFlags()
+	duration := time.Duration(interval) * time.Minute
+
+	for {
+		downloadSpeed, uploadSpeed, err := checkSpeed()
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		if downloadSpeed < downloadThreshold {
+			fmt.Printf("Download speed is below %.2f mbps", downloadThreshold)
+			currentDateTime := getCurrentDateTime()
+			messageContent := fmt.Sprintf("Uh oh! Download speed at %s was below %.2f mbps. Current download speed is %.2f mbps.", currentDateTime, downloadThreshold, downloadSpeed)
+			notify(messageContent)
+		}
+		if uploadMonitoring && uploadSpeed < uploadThreshold {
+			fmt.Printf("Upload speed is below %.2f mbps", uploadThreshold)
+			currentDateTime := getCurrentDateTime()
+			messageContent := fmt.Sprintf("Uh oh! Upload speed at %s was below %.2f mbps. Current upload speed is %.2f mbps.", currentDateTime, uploadThreshold, uploadSpeed)
+			notify(messageContent)
+		}
+		if once {
+			break
+		}
+		time.Sleep(duration)
+	}
+
 }
